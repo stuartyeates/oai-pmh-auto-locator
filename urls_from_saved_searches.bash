18b32e443be211e27ae70208e01d26b154a75179
Stuart Yeates <syeates@gmail.com>
2020-12-18 03:25:46 +0000

    dspace regexps, duplicate URLs containing other URLs

--- urls_from_saved_searches.bash
+++ urls_from_saved_searches.bash
@@ -9,7 +9,7 @@ LOG=${LOGDIR}/search_${TIMESTAMP}
 
 for STR in Google Bing Yandex; do
 	echo $STR;
-	 find ./source_data/ -name "*${STR}*.html" -exec cat '{}' \; | tr '" <>' '\012' | tr "'" '\012' | grep  "^https\?://[^/]\+/" | ./filter_out_common_websites.bash | sort --unique | \
+	 find ./source_data/ -name "*${STR}*.html" -exec cat '{}' \; | tr '" <>' '\012' | tr "'" '\012' | grep  "^https\?://[^/]\+/" | sed 's#^\(.*=http\(.\+\)\)$#\1\nhttp\2#' | ./filter_out_common_websites.bash | sort --unique | \
 		tee >(grep -h '\(/about/aboutThisPublishingSystem\|/user/register\|user/setLocale/NEW_LOCALE\|/about/editorialTeam$\|/about/submissions$\|/about/contact$\|/information/readers$\|/information/authors$\|/information/librarians$\)' | sed 's#\(/[^/]\+/[^/]\+\(?.*\)\?$\)#/oai#' | sort --unique  > ${LOG}_${STR}_ojs_system.urls ) \
 		>(grep -h '\(/index.php/\)'| sed 's#/\(index.php/[^/]\+/\).*#/\1oai#'  | sort --unique  > ${LOG}_${STR}_ojs_system2.urls ) \
 		>(grep -h '\(/article/downloadSuppFile/\|/article/viewFile/\|/article/download/\)'  | sort --unique  > ${LOG}_${STR}_ojs_files.urls ) \
